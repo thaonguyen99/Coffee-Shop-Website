47095e8098271b1fc0f7e2fb4d8eba82ad17eb49
thaonguyen99 <nhthau99@gmail.com>
2020-05-24 22:09:54 +0700

    front

---
+++ Server/view/javascript/main.js
@@ -0,0 +1,18 @@
+const backToTop = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction(){
+    if (window.scrollY > 300){
+        backToTop.style.display = "block";
+    }
+    else{
+        backToTop.style.display = "none";
+    }
+}
+
+backToTop.addEventListener("click", scrollUp);
+
+function scrollUp(){
+    window.scrollTo(0,0);
+}
