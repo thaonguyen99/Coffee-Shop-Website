e6469a8517f443f1f75705797b3def3fff50ddd8
thaonguyen99 <nhthau99@gmail.com>
2020-05-27 22:09:25 +0700

    change some css

--- Server/view/javascript/main.js
+++ Server/view/javascript/main.js
@@ -2,17 +2,16 @@ const backToTop = document.querySelector("#back-to-top-btn");
 
 window.addEventListener("scroll", scrollFunction);
 
-function scrollFunction(){
-    if (window.scrollY > 300){
-        backToTop.style.display = "block";
-    }
-    else{
-        backToTop.style.display = "none";
-    }
+function scrollFunction() {
+  if (window.scrollY > 300) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
 }
 
 backToTop.addEventListener("click", scrollUp);
 
-function scrollUp(){
-    window.scrollTo(0,0);
+function scrollUp() {
+  window.scrollTo(0, 0);
 }
